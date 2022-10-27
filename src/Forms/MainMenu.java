6244803b6390e6830d7a46d7bd9a75cbe157d4bf
eydriyan <115596606+eydriyan@users.noreply.github.com>
2022-10-27 09:46:45 +0800

    main menu changes
    main menu changes

--- src/Forms/MainMenu.java
+++ src/Forms/MainMenu.java
@@ -39,11 +39,13 @@ public class MainMenu extends javax.swing.JFrame implements IGameCallback, IResu
         PlayBtn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(580, 510));
         setResizable(false);
+        setSize(new java.awt.Dimension(0, 0));
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("A Game");
+        jLabel1.setText("Block Evader");
 
         QuitBtn.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         QuitBtn.setText("Quit");
@@ -65,28 +67,27 @@ public class MainMenu extends javax.swing.JFrame implements IGameCallback, IResu
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(125, 125, 125)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(88, 88, 88)
-                        .addComponent(PlayBtn)
-                        .addGap(71, 71, 71)
-                        .addComponent(QuitBtn)))
-                .addContainerGap(91, Short.MAX_VALUE))
+                    .addComponent(QuitBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 257, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(PlayBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 257, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(160, 160, 160))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(130, 130, 130)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 312, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(139, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(41, 41, 41)
+                .addContainerGap(135, Short.MAX_VALUE)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 84, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(QuitBtn)
-                    .addComponent(PlayBtn))
-                .addGap(81, 81, 81))
+                .addGap(91, 91, 91)
+                .addComponent(PlayBtn)
+                .addGap(18, 18, 18)
+                .addComponent(QuitBtn)
+                .addGap(130, 130, 130))
         );
 
         pack();
